7fb920cdaca6c7487836f084f2ba9de2de7fc414
Talipov S.N <proffix4@gmail.com>
2022-04-15 17:55:30 +0600

    Add files via upload

---
+++ astra/old/far2l_64-2.2-astra_install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt install libglib2.0-dev libwxgtk3.0-dev libgtkmm-3.0-dev uuid-dev libssl-dev libsecret-1-dev sshfs gvfs-libs gvfs-backends gvfs-fuse
+sudo dpkg -i far2l_64-2.2-astra.deb
